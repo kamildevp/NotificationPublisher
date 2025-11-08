1bb295dc92a86fe1bed116bc2483359b46c5e15e
kamildevp <kamil.k52645@gmail.com>
2025-11-09 00:53:01 +0100

    Moved delivery scheduling logic to DeliveryFactory

--- src/Notification/Delivery/Application/EventSubscriber/NotificationCreatedSubscriber.php
+++ src/Notification/Delivery/Application/EventSubscriber/NotificationCreatedSubscriber.php
@@ -4,26 +4,18 @@ declare(strict_types=1);
 
 namespace App\Notification\Delivery\Application\EventSubscriber;
 
-use App\Notification\Delivery\Domain\Entity\Delivery;
+use App\Notification\Delivery\Domain\Factory\DeliveryFactory;
 use App\Notification\Delivery\Domain\Repository\DeliveryRepositoryInterface;
 use App\Notification\Management\Domain\Event\NotificationCreatedEvent;
-use App\Notification\Shared\Domain\Entity\ValueObject\DeliveryId;
-use App\Notification\Delivery\Domain\ValueObject\CommunicationChannel;
-use Ramsey\Uuid\Uuid;
-use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class NotificationCreatedSubscriber implements EventSubscriberInterface
 {
-    /**
-     * @param iterable<NotificationDeliveryPolicyInterface> $notificationDeliveryPolices
-     */
     public function __construct(
         private DeliveryRepositoryInterface $deliveryRepository,
         private EventDispatcherInterface $eventDispatcher,
-        #[AutowireIterator('notification_delivery_policy_service')]
-        private iterable $notificationDeliveryPolices,
+        private DeliveryFactory $deliveryFactory,
     ) 
     {
 
@@ -31,36 +23,14 @@ class NotificationCreatedSubscriber implements EventSubscriberInterface
 
     public function onNotificationCreated(NotificationCreatedEvent $event): void
     {
-        foreach(CommunicationChannel::cases() as $communicationChannel)
-        {
-            $policiesPassed = true;
-            foreach($this->notificationDeliveryPolices as $notificationDeliveryPolicy)
-            {
-                if(!$notificationDeliveryPolicy->canNotificationBeDelivered(
-                    $event->getNotificationRecipient(), 
-                    $event->getNotificationType(), 
-                    $event->getNotificationData(), 
-                    $communicationChannel
-                )){
-                    $policiesPassed = false;
-                    break;
-                }
-            }
-            
-            if(!$policiesPassed){
-                continue;
-            }
+        $deliveries = $this->deliveryFactory->scheduleNotificationDeliveries(
+            $event->getNotificationId(),
+            $event->getNotificationRecipient(),
+            $event->getNotificationType(),
+            $event->getNotificationData()
+        );
 
-            $deliveryId = new DeliveryId(Uuid::uuid4()->toString());
-            $delivery = Delivery::schedule(
-                $deliveryId,
-                $event->getNotificationId(),
-                $event->getNotificationType(),
-                $communicationChannel,
-                $event->getNotificationData(),
-                $event->getNotificationRecipient()
-            );
-
+        foreach($deliveries as $delivery){
             $this->deliveryRepository->save($delivery);
             foreach($delivery->pullDomainEvents() as $event){
                 $this->eventDispatcher->dispatch($event);
--- tests/Unit/Notification/Delivery/Application/EventSubscriber/NotificationCreatedSubscriberTest.php
+++ tests/Unit/Notification/Delivery/Application/EventSubscriber/NotificationCreatedSubscriberTest.php
@@ -6,26 +6,22 @@ namespace App\Tests\Unit\Notification\Delivery\Application\EventSubscriber;
 
 use App\Notification\Delivery\Application\EventSubscriber\NotificationCreatedSubscriber;
 use App\Notification\Delivery\Domain\Entity\Delivery;
-use App\Notification\Delivery\Domain\Event\DeliveryScheduledEvent;
+use App\Notification\Delivery\Domain\Factory\DeliveryFactory;
 use App\Notification\Delivery\Domain\Repository\DeliveryRepositoryInterface;
-use App\Notification\Delivery\Domain\Service\NotificationDeliveryPolicyInterface;
-use App\Notification\Shared\Domain\Entity\ValueObject\DeliveryId;
 use App\Notification\Shared\Domain\Entity\ValueObject\NotificationId;
-use App\Notification\Delivery\Domain\ValueObject\CommunicationChannel;
-use App\Notification\Delivery\Domain\ValueObject\DeliveryStatus;
 use App\Notification\Management\Domain\Event\NotificationCreatedEvent;
 use App\Notification\Shared\Domain\ValueObject\Email;
 use App\Notification\Shared\Domain\ValueObject\NotificationType;
 use App\Notification\Shared\Domain\ValueObject\Phone;
 use App\Notification\Shared\Domain\ValueObject\Recipient;
-use ArrayIterator;
+use App\Shared\Domain\Event\DomainEventInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class NotificationCreatedSubscriberTest extends TestCase
 {
-    private NotificationDeliveryPolicyInterface&MockObject $deliveryPolicyMock;
+    private DeliveryFactory&MockObject $deliveryFactoryMock;
     private DeliveryRepositoryInterface&MockObject $deliveryRepositoryMock;
     private EventDispatcherInterface&MockObject $eventDispatcherMock;
     private NotificationCreatedSubscriber $service;
@@ -33,16 +29,16 @@ class NotificationCreatedSubscriberTest extends TestCase
     protected function setUp(): void
     {
         $this->eventDispatcherMock = $this->createMock(EventDispatcherInterface::class);
-        $this->deliveryPolicyMock = $this->createMock(NotificationDeliveryPolicyInterface::class);
+        $this->deliveryFactoryMock = $this->createMock(DeliveryFactory::class);
         $this->deliveryRepositoryMock = $this->createMock(DeliveryRepositoryInterface::class);
         $this->service = new NotificationCreatedSubscriber(
             $this->deliveryRepositoryMock,
             $this->eventDispatcherMock,
-            new ArrayIterator([$this->deliveryPolicyMock]),
+            $this->deliveryFactoryMock,
         );
     }
     
-    public function testOnNotificationCreatedCreatesDeliveryForEachCommunicationChannelWhenDeliveryPoliciesPass(): void
+    public function testOnNotificationCreatedSchedulesNotificationDeliveriesAndDispatchesEvents(): void
     {
         $event = new NotificationCreatedEvent(
             new NotificationId('242f924c-3910-45da-9291-772bcaa4fc6b'),
@@ -51,113 +47,32 @@ class NotificationCreatedSubscriberTest extends TestCase
             ['message' => 'test']
         );
 
-        $communicationChannels = CommunicationChannel::cases();
+        $eventMock = $this->createMock(DomainEventInterface::class);
+        $deliveryMock = $this->createMock(Delivery::class);
+        $deliveryMock
+            ->expects($this->once())
+            ->method('pullDomainEvents')
+            ->willReturn([$eventMock]);
 
-        $this->deliveryPolicyMock
-            ->expects($this->exactly(count($communicationChannels)))
-            ->method('canNotificationBeDelivered')
-            ->willReturnMap(array_map(
-                fn($communicationChannel) => [
-                    $event->getNotificationRecipient(),
-                    $event->getNotificationType(),
-                    $event->getNotificationData(),
-                    $communicationChannel,
-                    true
-                ],
-                $communicationChannels
-            ));
+        $this->deliveryFactoryMock
+            ->method('scheduleNotificationDeliveries')
+            ->with(
+                $event->getNotificationId(),
+                $event->getNotificationRecipient(),
+                $event->getNotificationType(),
+                $event->getNotificationData(),
+            )
+            ->willReturn([$deliveryMock]);
 
-        $saveCallNr = 0;
         $this->deliveryRepositoryMock
-            ->expects($this->exactly(count($communicationChannels)))
+            ->expects($this->once())
             ->method('save')
-            ->with($this->callback(function($arg) use (&$saveCallNr, $event, $communicationChannels){
-                $saveCallNr++;
-                return $arg instanceof Delivery &&
-                    $arg->getId() instanceof DeliveryId &&
-                    $arg->getNotificationId() == $event->getNotificationId() &&
-                    $arg->getNotificationType() == $event->getNotificationType() &&
-                    $arg->getCommunicationChannel() == $communicationChannels[$saveCallNr-1] &&
-                    $arg->getNotificationData() == $event->getNotificationData() &&
-                    $arg->getRecipient() == $event->getNotificationRecipient() &&
-                    $arg->getStatus() == DeliveryStatus::PENDING;
-            }));
+            ->with($deliveryMock);
 
-        $dispatchCallNr = 0;
         $this->eventDispatcherMock
-            ->expects($this->exactly(count($communicationChannels)))
+            ->expects($this->once())
             ->method('dispatch')
-            ->with($this->callback(function($arg) use (&$dispatchCallNr, $event, $communicationChannels){
-                $dispatchCallNr++;
-                return $arg instanceof DeliveryScheduledEvent &&
-                    $arg->getDeliveryId() instanceof DeliveryId &&
-                    $arg->getNotificationId() == $event->getNotificationId() &&
-                    $arg->getNotificationType() == $event->getNotificationType() &&
-                    $arg->getCommunicationChannel() == $communicationChannels[$dispatchCallNr-1] &&
-                    $arg->getNotificationData() == $event->getNotificationData() &&
-                    $arg->getNotificationRecipient() == $event->getNotificationRecipient();
-            }));
-
-        $this->service->onNotificationCreated($event);
-    }
-
-    public function testOnNotificationCreatedSkipsDeliveryCreationForUnavailableChannel(): void
-    {
-        $event = new NotificationCreatedEvent(
-            new NotificationId('242f924c-3910-45da-9291-772bcaa4fc6b'),
-            NotificationType::INFO,
-            new Recipient('2a8045fd', new Email('user@example.com'), new Phone('+48213721372')),
-            ['message' => 'test']
-        );
-
-        $communicationChannels = CommunicationChannel::cases();
-        $unavailableChannel = CommunicationChannel::EMAIL;
-        $availableChannels = array_values(array_filter($communicationChannels, fn($communicationChannel) => $communicationChannel != $unavailableChannel));
-
-        $this->deliveryPolicyMock
-            ->expects($this->exactly(count($communicationChannels)))
-            ->method('canNotificationBeDelivered')
-            ->willReturnMap(array_map(
-                fn($communicationChannel) => [
-                    $event->getNotificationRecipient(),
-                    $event->getNotificationType(),
-                    $event->getNotificationData(),
-                    $communicationChannel,
-                    $communicationChannel != $unavailableChannel
-                ],
-                $communicationChannels
-            ));
-
-        $saveCallNr = 0;
-        $this->deliveryRepositoryMock
-            ->expects($this->exactly(count($availableChannels)))
-            ->method('save')
-            ->with($this->callback(function($arg) use (&$saveCallNr, $event, $availableChannels){
-                $saveCallNr++;
-                return $arg instanceof Delivery &&
-                    $arg->getId() instanceof DeliveryId &&
-                    $arg->getNotificationId() == $event->getNotificationId() &&
-                    $arg->getNotificationType() == $event->getNotificationType() &&
-                    $arg->getCommunicationChannel() == $availableChannels[$saveCallNr-1] &&
-                    $arg->getNotificationData() == $event->getNotificationData() &&
-                    $arg->getRecipient() == $event->getNotificationRecipient() &&
-                    $arg->getStatus() == DeliveryStatus::PENDING;
-            }));
-
-        $dispatchCallNr = 0;
-        $this->eventDispatcherMock
-            ->expects($this->exactly(count($availableChannels)))
-            ->method('dispatch')
-            ->with($this->callback(function($arg) use (&$dispatchCallNr, $event, $availableChannels){
-                $dispatchCallNr++;
-                return $arg instanceof DeliveryScheduledEvent &&
-                    $arg->getDeliveryId() instanceof DeliveryId &&
-                    $arg->getNotificationId() == $event->getNotificationId() &&
-                    $arg->getNotificationType() == $event->getNotificationType() &&
-                    $arg->getCommunicationChannel() == $availableChannels[$dispatchCallNr-1] &&
-                    $arg->getNotificationData() == $event->getNotificationData() &&
-                    $arg->getNotificationRecipient() == $event->getNotificationRecipient();
-            }));
+            ->with($eventMock);
 
         $this->service->onNotificationCreated($event);
     }
